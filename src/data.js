be9291171e9e306ccd889d71e9318cfc9fdde8bb
Linzell <85298676+Linzell@users.noreply.github.com>
2021-07-14 22:49:14 +0200

    Essai changements des liens

--- src/data.js
+++ src/data.js
@@ -13,7 +13,7 @@ export const projects = [
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "./project-2.png",
-    link: "%PUBLIC_URL%/",
+    link: "linzell.fr",
   },
   {
     title: "Illdly",
