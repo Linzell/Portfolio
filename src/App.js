47d06ae073c6a722137720a51e2c0a4736f4ca2e
Charlie Cohen <linzellart@gmail.com>
2021-07-20 00:27:14 +0200

    Update du site

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
+import Legal from "./components/Legal";
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
       <Skills />
       <Testimonials />
       <Contact />
+      <Legal />
     </main>
   )
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,12 +7,12 @@ import { skills } from "../data";
 export default function Skills() {
     return (
         <section id="skills">
-          <div className="container px-5 py-10 mx-auto">
+          <div className="container py-10 mx-auto">
             <div className="text-center mb-20">
               <ChipIcon className="w-10 inline-block mb-4" />
-              <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
+              <h2 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
                 Compétences &amp; Technologies
-              </h1>
+              </h2>
               <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit
                 ipsa delectus eum quo voluptas aspernatur accusantium distinctio
